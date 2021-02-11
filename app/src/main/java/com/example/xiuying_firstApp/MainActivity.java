6d4ec7d9bc6fbcbcc0f0ca2c3276247320bcb7a3
xiuyingsi <321801078@qq.com>
2021-02-12 00:21:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xiuying_firstApp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.xiuying_firstApp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
